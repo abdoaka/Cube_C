e98060aa462d32767748f28b199401e01ce37079
Abdelmouiz Saadani <absaadan@c1r4p10.1337.ma>
2025-08-22 13:18:06 +0100

    changes norm

--- rendering/src/cast_vertical_ray.c
+++ rendering/src/cast_vertical_ray.c
@@ -49,41 +49,42 @@ static void	setup_vertical_intercept(t_game *game, t_ray *ray,
 	data->next_y = data->y_intercept;
 }
 
-static void step_vertical_ray(t_game *game, t_ray *ray, t_vertical_ray_data *data)
+static void	step_vertical_ray(t_game *game, t_ray *ray,
+		t_vertical_ray_data *data)
 {
-    double check_x;
+	double	check_x;
 
-    while (data->next_x >= 0 && data->next_x < data->max_width
-        && data->next_y >= 0 && data->next_y < data->max_height)
-    {
-        check_x = data->next_x;
-        if (cos(ray->angle) < 0)
-            check_x -= 1;
-        if (is_wall(game, check_x, data->next_y))
-        {
-            data->result_x = data->next_x / data->tile_size;
-            data->result_y = data->next_y / data->tile_size;
-            data->wall_found = 1;
-            return;
-        }
-        data->next_x += data->x_step;
-        data->next_y += data->y_step;
-    }
-    data->result_x = -1;
-    data->result_y = -1;
-    data->wall_found = 0;
+	while (data->next_x >= 0 && data->next_x < data->max_width
+		&& data->next_y >= 0 && data->next_y < data->max_height)
+	{
+		check_x = data->next_x;
+		if (cos(ray->angle) < 0)
+			check_x -= 1;
+		if (is_wall(game, check_x, data->next_y))
+		{
+			data->result_x = data->next_x / data->tile_size;
+			data->result_y = data->next_y / data->tile_size;
+			data->wall_found = 1;
+			return ;
+		}
+		data->next_x += data->x_step;
+		data->next_y += data->y_step;
+	}
+	data->result_x = -1;
+	data->result_y = -1;
+	data->wall_found = 0;
 }
 
-void cast_vertical_ray(t_game *game, t_ray *ray, double *v_x, double *v_y)
+void	cast_vertical_ray(t_game *game, t_ray *ray, double *v_x, double *v_y)
 {
-    t_vertical_ray_data data;
+	t_vertical_ray_data	data;
 
-    data.tile_size = calculate_tile_size(game);
-    if (data.tile_size < 4)
-        data.tile_size = 4;
-    calculate_map_boundaries(game, &data);
-    setup_vertical_intercept(game, ray, &data);
-    step_vertical_ray(game, ray, &data);
-    *v_x = data.result_x;
-    *v_y = data.result_y;
+	data.tile_size = calculate_tile_size(game);
+	if (data.tile_size < 4)
+		data.tile_size = 4;
+	calculate_map_boundaries(game, &data);
+	setup_vertical_intercept(game, ray, &data);
+	step_vertical_ray(game, ray, &data);
+	*v_x = data.result_x;
+	*v_y = data.result_y;
 }
